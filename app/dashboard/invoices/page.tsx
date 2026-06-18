9a47e8b4195a94d138a08641fd03018daf26a3e1
Denis Eshtrekov <d.eshtrekov@ya.ru>
2026-06-18 12:46:35 +0300

    test

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from "react";
 
+import { fetchInvoicesPages } from "@/app/lib/data";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { lusitana } from "@/app/ui/fonts";
 import Table from "@/app/ui/invoices/table";
-import { fetchInvoicesPages } from "@/app/lib/data";
 
 export default async function Page(props: {
   searchParams?: Promise<{
